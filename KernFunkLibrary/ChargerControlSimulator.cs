818e98237aa4325e41e52ffe7ba9a30c2819f1a6
Einar Hansson <einarjorgen.hansson@gmail.com>
2020-10-21 09:08:55 +0200

    feat: IChargerControl og Charger

---
+++ KernFunkLibrary/ChargerControlSimulator.cs
@@ -0,0 +1,25 @@
+﻿using KernFunkLibrary.UsbSimulator;
+
+namespace KernFunkLibrary
+{
+    public class ChargerControlSimulator : IChargerControl
+    {
+        private IDisplay _display;
+        private IUsbCharger _iUsbCharger;
+        public bool IsConnected { get; }
+        public void StartCharge()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void StopCharge()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void HandleCurrentEvent(object sender, CurrentEventArgs e)
+        {
+
+        }
+    }
+}
