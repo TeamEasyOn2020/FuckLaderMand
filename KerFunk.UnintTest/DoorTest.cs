022819c368d356f9cdecd57af287bc3a836b8043
Andreas Hermann <andreashermann170@gmail.com>
2020-10-27 10:58:15 +0100

    test: UnlockDoor og LockDoor testet

---
+++ KerFunk.UnintTest/DoorTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KernFunkLibrary;
+using NUnit.Framework;
+
+namespace KerFunk.UnintTest
+{
+    [TestFixture]
+    class DoorTest
+    {
+        private IDoor _uut;
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new DoorSimulator();
+        }
+
+        [Test]
+        public void LockDoorDoorClosedTrueDoorOpenFalse()
+        {
+            _uut.LockDoor();
+            Assert.That(_uut.DoorOpen==false && _uut.DoorClosed==true);
+            
+        }
+
+        [Test]
+        public void UnlockDoorDoorClosedFalseDoorOpenTrue()
+        {
+            _uut.UnlockDoor();
+            Assert.That(_uut.DoorOpen == true && _uut.DoorClosed == false);
+
+        }
+
+
+    }
+}
