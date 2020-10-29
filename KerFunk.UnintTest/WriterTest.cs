e34970b868b669b52a6f4af2f9ad84572caacebb
Einar Jörgen Hansson <au626702@uni.au.dk>
2020-10-29 14:18:04 +0100

    test:Added writer test

--- KerFunk.UnintTest/WriterTest.cs
+++ KerFunk.UnintTest/WriterTest.cs
@@ -1,47 +1,46 @@
-﻿//using System;
-//using System.IO;
-//using System.Linq;
-//using System.Text;
-//using KernFunkLibrary;
-//using NSubstitute;
-//using NUnit.Framework;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using KernFunkLibrary;
+using NSubstitute;
+using NUnit.Framework;
 
-//namespace KerFunk.UnintTest
-//{
-//    public class WriterTest
-//    {
-//        private WriterSimulator _uut;
+namespace KerFunk.UnintTest
+{
+    public class WriterTest
+    {
+        private WriterSimulator _uut;
 
-//        [SetUp]
-//        public void Setup()
-//        {
-//            _uut = new WriterSimulator();
-            
-//        }
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new WriterSimulator("/LogFile.txt");
+
+        }
 
 
-//        [TestCase(null)]
-//        [TestCase("")]
-//        [TestCase("Door Locked")]
-//        [Test]
-//        public void WriteLine_message_WrittenOutputWasMessage(string message)
-//        {
-//            //Setup
-//            var writer = Substitute.ForPartsOf<WriterSimulator>();
-//            var strem = new MemoryStream();
-//            StreamWriter streamWriterTest = new StreamWriter(strem);
-//            writer.LogFile = "LogFile.txt";
-//            writer.GetLogFile().Returns(streamWriterTest);
-//            string expected = message + "\r\n";
-            
-//            //Act
-//            writer.WriteLine(message);
+        
+        [Test]
+        public void WriteLine_FileWasCreated()
+        {
+            File.Delete(_uut.LogFile);
+           _uut.WriteLine("Test");
 
-//            //Assert
-//            string actualOutput = Encoding.UTF8.GetString(strem.ToArray());
-//            Assert.AreEqual(expected, actualOutput);
-//        }
-//    }
+           Assert.True(File.Exists(_uut.LogFile));
+        }
 
-    
-//}
+        
+        [TestCase("")]
+        [TestCase("Door Locked")]
+        [Test]
+        public void WriteLine_message_MessageWrittenToFile(string message)
+        {
+            _uut.WriteLine(message);
+            var lastLine = File.ReadLines(_uut.LogFile).Last();
+            Assert.AreEqual(message, lastLine);
+        }
+    }
+
+
+}
